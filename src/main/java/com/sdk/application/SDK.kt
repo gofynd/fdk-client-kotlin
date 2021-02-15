5ef4c3a06976d3b293102fd899bf8085100c6032
Jigar Dafda <jigar.dafda@gmail.com>
2021-02-15 18:36:39 +0000

    [Auto Generated] build_and_push

--- src/main/java/com/sdk/application/SDK.kt
+++ src/main/java/com/sdk/application/SDK.kt
@@ -4,10 +4,10 @@ import android.app.Application
 
 object SDK{
 
-    var applicationID: String?=null
-    var applicationToken: String?=null
-    var domain: String?:null
-    var application: Application?=null
+    var applicationID: String? = null
+    var applicationToken: String? = null
+    var domain: String? = null
+    var application: Application? = null
 
     fun init(applicationID:String, applicationToken: String, domain:String="https://api.fynd.com",application:Application){
         this.applicationID=applicationID
