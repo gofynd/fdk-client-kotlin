a699cb1d27c51d9b199cae5bc6dea68965990fdc
Jigar Dafda <jigar.dafda@gmail.com>
2023-11-24 18:04:32 +0000

    [Auto Generated] 1.3.9

--- src/main/java/com/sdk/common/ApplicationHeaderInterceptor.kt
+++ src/main/java/com/sdk/common/ApplicationHeaderInterceptor.kt
@@ -27,7 +27,7 @@ class ApplicationHeaderInterceptor(
                 header("x-location-detail", HttpClient.gson.toJson(it))
             }
             header("x-currency-code", currencyCode)
-            header("x-fp-sdk-version", "1.3.8")
+            header("x-fp-sdk-version", "1.3.9")
             val bearerToken =
                 Base64.encodeToString(
                     "$applicationId:$applicationToken".toByteArray(),
