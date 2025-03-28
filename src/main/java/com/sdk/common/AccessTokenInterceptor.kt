887b6c3056b978516e54ab0fed63217bacc39de0
Jigar Dafda <jigar.dafda@gmail.com>
2025-03-28 18:22:03 +0000

    [Auto Generated] 0.0.1111-3.beta.1

--- src/main/java/com/sdk/common/AccessTokenInterceptor.kt
+++ src/main/java/com/sdk/common/AccessTokenInterceptor.kt
@@ -20,7 +20,7 @@ class AccessTokenInterceptor(
             }
 
             if (!original.headers.names().contains("x-fp-sdk-version")) {
-                header("x-fp-sdk-version", "0.0.1111-1.beta.2")
+                header("x-fp-sdk-version", "0.0.1111-3.beta.1")
             }
 
             if (!original.headers.names().contains("x-currency-code")) {
